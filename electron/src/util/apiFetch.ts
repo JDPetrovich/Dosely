4df59edbb219b4a2308e011bd9109c447ec3b89e
João David Petrovich <joaopetrovich9393@gmail.com>
2026-04-07 15:47:32 -0300

    criado um client para facilitar a chamada da api

---
+++ electron/src/util/apiFetch.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { CookieJar } from "tough-cookie";
+import { wrapper } from "axios-cookiejar-support";
+
+const jar = new CookieJar();
+
+const API_KEY = process.env.API_KEY
+const KEY = process.env.KEY
+
+const api = wrapper(axios.create({
+    baseURL: API_KEY,
+    jar,
+    withCredentials: true,
+    headers: {
+        "x-api-key": KEY,
+    }
+}));
+
+export async function apiFetch(path: string, options: any = {}) {
+    try {
+        const res = await api({
+            url: path,
+            method: options.method || "GET",
+            data: options.body ? JSON.parse(options.body) : undefined,
+        });
+
+        return res.data;
+    } catch (error: any) {
+        return {
+            sucesso: false,
+            mensagem: error.response?.data?.mensagem || error.message,
+        };
+    }
+}
